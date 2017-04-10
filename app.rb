253605c81526d5295b56e8797448a8e37e1a28f1
Ishan Kanade <ekanade@gmail.com>
2017-04-10 11:17:45 +0530

    Functionality for string reversal in a basic sinatra app

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'bundler/setup'
+
+Bundler.require(:default, :test, :development)
+
+require 'active_support/all'
+
+class App < Sinatra::Base
+
+  get '/'do 
+    "Psst! You are not allowed here."
+  end
+
+  get '/reverse' do 
+    request.body.rewind
+    content_type :json
+    data = JSON.parse(request.body.read).with_indifferent_access
+  
+    result = {
+      meta_info: {
+        served_at: Time.now,
+        served_by: "#{`hostname`}".chomp
+      },
+      output: "#{data[:input]}".mb_chars.reverse.to_s 
+    }
+  
+    result.to_json
+  end
+end
