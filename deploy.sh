0df6798dd3eb14b347eefc15fb0fdb837ac6d7c3
Ishan Kanade <ekanade@gmail.com>
2017-04-11 21:20:30 +0530

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,69 @@
 #!/usr/bin/env bash
 
-# more bash-friendly output for jq
+set -e
+
 JQ="jq --raw-output --exit-status"
 
 configure_aws_cli(){
-	aws --version
-	aws configure set default.region us-east-1
-	aws configure set default.output json
+    aws --version
+    aws configure set default.region us-east-1
+    aws configure set default.output json
+}
+
+deploy_image() {
+    # get the authorization code and login to aws ecr
+    eval $(aws ecr get-login --region us-east-1)
+    docker tag reversible:$CIRCLE_SHA1 $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reversible:$CIRCLE_SHA1
+    docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reversible:$CIRCLE_SHA1
+}
+
+make_task_def(){
+    task_template='[
+        {
+            "name": "reversible-app",
+            "image": "%s.dkr.ecr.us-east-1.amazonaws.com/reversible:%s",
+            "essential": true,
+            "memory": 300,
+            "cpu": 1,
+            "portMappings": [
+                {
+                    "containerPort": "80",
+                    "hostPort": "80",
+                    "protocol": "tcp"
+                }
+            ]
+        }
+    ]'
+    
+    task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
+}
+
+register_definition() {
+
+    if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
+        echo "Revision: $revision"
+    else
+        echo "Failed to register task definition"
+        return 1
+    fi
+
 }
 
 deploy_cluster() {
-
-    family="reversible-task-family"
-
+    
+    
     make_task_def
     register_definition
     
-    if [[ $(aws ecs update-service --cluster reversible-cluster --service reversible-service --task-definition $revision | \
+
+    if [[ $(aws ecs update-service --cluster $cluster_name --service $service_name --task-definition $revision | \
                    $JQ '.service.taskDefinition') != $revision ]]; then
         echo "Error updating service."
         return 1
     fi
 
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
     for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster reversible-cluster --services reversible-service | \
+        if stale=$(aws ecs describe-services --cluster $cluster_name --services $service_name | \
                        $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
             echo "Waiting for stale deployments:"
             echo "$stale"
@@ -37,44 +75,13 @@ deploy_cluster() {
     done
     echo "Service update took too long."
     return 1
-}
-
-make_task_def(){
-	task_template='[
-		{
-			"name": "reversible-app",
-			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/reversible:%s",
-			"essential": true,
-			"memory": 200,
-			"cpu": 10,
-			"portMappings": [
-				{
-					"containerPort": 80,
-					"hostPort": 80
-				}
-			]
-		}
-	]'
-	
-	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
-}
-
-push_ecr_image(){
-	eval $(aws ecr get-login --region us-east-1)
-	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/reversible:$CIRCLE_SHA1
-}
-
-register_definition() {
-
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
-        echo "Revision: $revision"
-    else
-        echo "Failed to register task definition"
-        return 1
-    fi
 
 }
+
+family=reversible-task-family
+service_name=reversible-service
+cluster_name=reversible-cluster
 
 configure_aws_cli
-push_ecr_image
+deploy_image
 deploy_cluster
