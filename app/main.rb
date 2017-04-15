466a2cdea6649695e0222fc73e11a0e834ee3bee
Ishan Kanade <ekanade@gmail.com>
2017-04-15 16:21:58 +0530

    Error handling for missing input

--- app/main.rb
+++ app/main.rb
@@ -5,18 +5,35 @@ class Reversible < Sinatra::Base
   end
 
   get '/reverse' do 
-    request.body.rewind
     content_type :json
-    data = JSON.parse(request.body.read).with_indifferent_access
-  
-    result = {
-      meta_info: {
-        served_at: Time.now,
-        served_by: "#{`hostname`}".chomp
-      },
-      output: "#{data[:input]}".mb_chars.reverse.to_s 
-    }
-  
-    result.to_json
+    request.body.rewind
+
+    data = 
+      if request.body.read.empty?
+        if  request.params.present? && request.params["input"].present?
+          { input: request.params["input"] }.with_indifferent_access
+        end
+      else
+        request.body.rewind
+        JSON.parse(request.body.read).with_indifferent_access
+      end
+
+    if data.present?
+      result = {
+        meta_info: {
+          served_at: Time.now,
+          served_by: "#{`hostname`}".chomp
+        },
+        output: "#{data[:input]}".mb_chars.reverse.to_s 
+      }
+      result.to_json
+    else
+      status = 422
+    end 
   end
+
+  error 422 do
+    {"error": "Provide a valid Input string"}.to_json
+  end
+
 end
